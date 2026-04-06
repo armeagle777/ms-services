f040f9f2fab93573a3009be60a30c8d82457ef30
Tigran Yeranyan <tigran.e@purlin.com>
2026-04-06 22:32:58 +0400

    feat: Employee Contracts implementation

--- src/Core/RevenueCommittee/RevenueCommittee.service.ts
+++ src/Core/RevenueCommittee/RevenueCommittee.service.ts
@@ -6,9 +6,10 @@ import { XMLParser } from 'fast-xml-parser';
 
 import { CompanyObligationsQueryDto } from 'src/API/DTO/Tax/tax.dto';
 import {
+   EmploymentContractResponse,
    TaxObligationsResponse,
    TaxPersonObligationsResponse,
-} from 'src/Core/RevenueCommittee/interfaces/tax.interfaces';
+} from './RevenueCommittee.types';
 import { RevenueCommitteeIntegration } from 'src/Infrustructure/Services/RevenueCommitteeIntegration/RevenueCommittee.integration';
 
 @Injectable()
@@ -65,6 +66,22 @@ export class RevenueCommitteeService {
       const responseBody = jsonObj?.Envelope?.Body?.Response?.ResponseBody || {};
 
       return responseBody as TaxPersonObligationsResponse;
+   }
+
+   async getEmploymentContracts(ssn: string): Promise<EmploymentContractResponse> {
+      const ekengRequestProps = { employee_ssn: ssn };
+      const options = this.revenueCommittee.buildRequestOptions(
+         '/employment_contract/v1',
+         ekengRequestProps,
+      );
+
+      const response = await firstValueFrom(this.httpService.request(options));
+      const data = response.data;
+      return data;
+
+      // return data?.ssn_obligations_response?.responseStatus?.statusCode === 1
+      //    ? (data.ssn_obligations_response as TaxObligationsResponse)
+      //    : null;
    }
 
    private formatObligationsXmlData(ssn: string) {
